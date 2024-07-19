661485d991edafd0188f1d72e77d5af5d7cbe29d
rakshitbagait <rbagait2018@gmail.com>
2024-07-19 19:58:19 +0530

    first program is of average of numbers given by the user

---
+++ Basic_C_Programs/AverageOfNumbers.c
@@ -0,0 +1,18 @@
+// To calculate the average of numbers using iterarion
+#include<stdio.h>
+int main(){
+    int count=0, sum=0, num, average,terms;
+    printf("enter the number of numbers you want");
+    scanf("%d",&terms);
+
+    while(count<terms){
+        printf("enter the number");
+        scanf("%d\n",&num);
+        sum =sum+num;
+        count++;
+    }
+    average =sum/terms;
+    printf("The average of the number is %d",average);
+    
+
+}
