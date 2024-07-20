089a6aaa9afc6fc95192e4bd9885025b844fcfaa
rakshitbagait <rbagait2018@gmail.com>
2024-07-20 21:42:00 +0530

    new pattern files added

---
+++ Loops/pattern1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n ;
+    printf("enter the num");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int i =1;i<5;i++){
+            printf("*");
+    
+        }
+    printf("\n");
+    }
+    return 0;
+}
---
+++ Loops/pattern5.c
@@ -0,0 +1,19 @@
+// 1
+// 12
+// 123
+// 1234
+#include<stdio.h>
+int main(){
+    for(int i=1; i <=4;i++){
+    for(int j=1; j<=i;j++)
+    {
+        printf("%d",j);
+
+    }
+    printf("\n");
+    }
+    
+    return 0;
+    
+
+}
