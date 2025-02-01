a4078f82efc5a0fa30dc39132679676ee22c927c
rakshitbagait <rbagait2018@gmail.com>
2025-02-01 23:04:09 +0530

    removing duplicates

--- gfg/ARRAYS/6.c
+++ gfg/ARRAYS/6.c
@@ -3,23 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main(void) 
+int main(void)
 {
     int n;
     printf("ENter the nubmer of terms in array");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-	int arr[n];
-    for(int i =0; i<n;i++)
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-        printf("Enter the element %d",i+1);
-        scanf("%d",&arr[i]);
-
+        printf("Enter the element %d", i + 1);
+        scanf("%d", &arr[i]);
     }
 
     // using quick sort
-        for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n - 1; j++)
         {
@@ -32,7 +30,7 @@ int main(void)
         }
     }
 
-    printf("%d,%d,%d",arr[n-1],arr[n-2],arr[n-3]);
+    printf("%d,%d,%d", arr[n - 1], arr[n - 2], arr[n - 3]);
 
-	return 0;
+    return 0;
 }
--- gfg/ARRAYS/8.c
+++ gfg/ARRAYS/8.c
@@ -3,13 +3,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main(void) 
+int main(void)
 {
-	int n; 
+    int n;
     printf("enter the number of elements in array");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i =0; i<size;)	
-	return 0;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element");
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("The original array\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d,", arr[i]);
+    }
+
+    int a = n;
+    for (int i = 0; i <n; i++)
+    {
+        if (arr[i] == arr[i+1])
+        {
+            for(int j=1;j<n;j++)
+            {
+                arr[j]=arr[j+1];
+
+            }
+            a--;
+        }
+    }
+    printf("The array after removal");
+
+    for (int i = 0; i <a; i++)
+    {
+        printf("%d,", arr[i]);
+    }
+    return 0;
 }
